85ed5f5715da95f2b095f47241c9e6abe9ec135a
Gideon Ovuzorie <gideon@macbooks-mbp.home>
2024-12-19 04:24:16 +0000

    Template

---
+++ cmd/web/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/iamYole/BookingApp/pkg/config"
+	"github.com/iamYole/BookingApp/pkg/handlers"
+	"github.com/iamYole/BookingApp/pkg/render"
+)
+
+const portNumber = ":8080"
+
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+	//change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Error creating template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	render.NewTemplate(&app)
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	fmt.Printf("Starting application on %s ...", portNumber)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Booking and Reservation
+
+This is the repository for my bookings and reservations project
+
+- Built in Go version 1.23.4
+- External Dependencies
+    - [nosurf]()
+    - [alex edwars scs session managment]()
+    - [chi router]()
